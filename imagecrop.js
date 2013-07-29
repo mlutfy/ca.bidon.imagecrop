95dee1834d64f7f86a084ec952d1ca193538a126
Mathieu Lutfy <mathieu@bidon.ca>
2013-07-29 11:49:59 -0400

    Partial implementation of the crop preview (nb: does not scale correctly).

--- imagecrop.js
+++ imagecrop.js
@@ -13,22 +13,69 @@ function imagecrop_civicrm_enable(selector) {
 
   // Create a hidden dialog that will popup when we click on the contact image
   var imgsrc = cj('.crm-contact_image img').attr('src');
+  var submit_text = ts('Crop', {context: 'ca.bidon.imagecrop'});
+
   cj('.crm-contact_image').append('<div style="display: none;" class="crm-imagecrop-dialog crm-container"></div>');
-  cj('.crm-contact_image .crm-imagecrop-dialog').append('<div class="crm-imagecrop-dialog-preview"><img style="width: 150px; height: 150px;" src="' + imgsrc + '" /><input type="submit" name="Submit" /></div>');
+  cj('.crm-contact_image .crm-imagecrop-dialog').append('<div class="crm-imagecrop-dialog-preview-pane"><div class="crm-imagecrop-dialog-preview-container"><img src="' + imgsrc + '" /></div></div>');
+  cj('.crm-contact_image .crm-imagecrop-dialog').append('<div class="crm-imagecrop-buttons"><input type="submit" name="crm-imagecrop-submit" value="' + submit_text + '" /></div>');
+  cj('.crm-contact_image .crm-imagecrop-dialog').append('<div class="crm-imagecrop-close"><a href="#">' + ts('Close') + '</a></div>');
   cj('.crm-contact_image .crm-imagecrop-dialog').append('<div class="crm-imagecrop-dialog-main"><img src="' + imgsrc + '" /></div>');
 
   cj('.crm-contact_image img').click(function() {
+    // c.f. https://github.com/tapmodo/Jcrop/blob/master/demos/tutorial3.html
+    // Create variables (in this scope) to hold the API and image size
+    var jcrop_api, boundx, boundy,
+  
+    // Grab some information about the preview pane
+    $preview = cj('.crm-imagecrop-dialog-preview-pane'),
+    $pcnt = cj('.crm-imagecrop-dialog-preview-container'),
+    $pimg = cj('.crm-imagecrop-dialog-preview-container img'),
+  
+    xsize = $pcnt.width(),
+    ysize = $pcnt.height();
+
     cj('.crm-imagecrop-dialog').dialog({
       title: ts("Test title", {domain: "ca.bidon.imagecrop"}),
       width: 1000,
-      height: 500,
+      height: 550,
+      aspectRatio: xsize / ysize,
       modal: true
     });
+  
+    cj('.crm-imagecrop-dialog .crm-imagecrop-close a').click(function() {
+      cj('.crm-imagecrop-dialog').dialog('close');
+    });
+  
+    function crmImageCropUpdatePreview(c) {
+      if (parseInt(c.w) > 0) {
+        var rx = xsize / c.w;
+        var ry = ysize / c.h;
+  
+        $pimg.css({
+          width: Math.round(rx * boundx) + 'px',
+          height: Math.round(ry * boundy) + 'px',
+          marginLeft: '-' + Math.round(rx * c.x) + 'px',
+          marginTop: '-' + Math.round(ry * c.y) + 'px'
+        });
+      }
+    }
 
     // Enable jcrop on the image shown
     jQuery('.crm-imagecrop-dialog .crm-imagecrop-dialog-main img').Jcrop({
       boxWidth: 640,
-      boxHeight: 480
+      boxHeight: 480,
+      onChange: crmImageCropUpdatePreview,
+      onSelect: crmImageCropUpdatePreview
+    }, function(){
+      // Use the API to get the real image size
+      var bounds = this.getBounds();
+      boundx = bounds[0];
+      boundy = bounds[1];
+      // Store the API in the jcrop_api variable
+      jcrop_api = this;
+
+      // Move the preview into the jcrop container for css positioning
+      $preview.appendTo(jcrop_api.ui.holder);
     });
   });
 }
