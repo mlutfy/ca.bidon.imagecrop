0f42e8322c036449075d0f1e1106330e1a9cf385
Mathieu Lutfy <mathieu@bidon.ca>
2013-08-19 16:37:03 -0400

    Finalise aspect ratio input and automatic conversions.

--- imagecrop.settings.js
+++ imagecrop.settings.js
@@ -44,18 +44,17 @@ cj(function($) {
       // found 16:9
       x = parseInt(found[1]);
       y = parseInt(found[2]);
-
     }
     else if (found[3] && found[4]) {
       // found 1.78 or 1.78:1
-      var x1 = parseInt(found[1]);
-      var x2 = parseInt(found[2]) / 100;
+      var x1 = parseInt(found[3]);
+      var x2 = parseInt(found[4]) / 100;
 
-      x = x1 * x2;
+      x = x1 + x2; // ex: 1 + 0.78
       y = 1;
 
-      if (found[3]) {
-        y = parseInt(found[3]);
+      if (found[6]) {
+        y = parseInt(found[6]);
       }
     }
 
@@ -63,13 +62,62 @@ cj(function($) {
     return Math.round(x / y * 1000) / 1000;
   }
 
-  $('input#croparea_x').change(function() {
-    var ratio = civicrm_imagecrop_parse_aspect_ratio();
+  function civicrm_imagecrop_calculate_x_from_y(selector_x, selector_y) {
+    $(selector_x).change(function() {
+      var ratio = civicrm_imagecrop_parse_aspect_ratio();
 
-    if (ratio) {
-      console.log('yay!');
-      console.log(ratio);
-    }
-  });
+      if (ratio) {
+        var x = parseInt($(this).val());
+        var y = parseInt($(selector_y).val());
+
+        // If the field was blanked out, do not do anything
+        if (! x) {
+          return;
+        }
+
+        // The value was already entered, and user might be adjusting the rounding.
+        if (y) {
+          return;
+        }
+
+        y = Math.round(x / ratio);
+        $(selector_y).val(y);
+      }
+    });
+  }
+
+  function civicrm_imagecrop_calculate_y_from_x(selector_x, selector_y) {
+    $(selector_y).change(function() {
+      var ratio = civicrm_imagecrop_parse_aspect_ratio();
+
+      if (ratio) {
+        var y = parseInt($(this).val());
+        var x = parseInt($(selector_x).val());
+
+        // If the field was blanked out, do not do anything
+        if (! y) {
+          return;
+        }
+
+        // The value was already entered, and user might be adjusting the rounding.
+        if (x) {
+          return;
+        }
+
+        x = Math.round(y * ratio);
+        $(selector_x).val(x);
+      }
+    });
+  }
+
+  function civicrm_imagecrop_calculate_aspect_ratio(selector_x, selector_y) {
+    civicrm_imagecrop_calculate_x_from_y(selector_x, selector_y);
+    civicrm_imagecrop_calculate_y_from_x(selector_x, selector_y);
+  }
+
+  civicrm_imagecrop_calculate_aspect_ratio('input#croparea_x', 'input#croparea_y');
+  civicrm_imagecrop_calculate_aspect_ratio('input#output_x', 'input#output_y');
+  civicrm_imagecrop_calculate_aspect_ratio('input#min_width', 'input#min_height');
+  civicrm_imagecrop_calculate_aspect_ratio('input#max_width', 'input#max_height');
 });
 
